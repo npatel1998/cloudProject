34103b4b5fa1878d1f713cf50417595b620f479b
Nisarg Patel <nisarg.patel@hitachids.com>
2025-12-22 15:04:24 +0530

    fixed infra

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -5,9 +5,9 @@ import { DynamoStack } from '../lib/dynamo_stack';
 import * as controller from '../controller.json';
 const app = new cdk.App();
 
+console.log('controller:', controller);
 
 const account ="137345587738";
-
 const region ='ap-south-1';
 
 if (!account) {
@@ -17,25 +17,27 @@ if (!account) {
 }
 
 const env: cdk.Environment = { account, region };
-let iamStack: IamStack | undefined;
-let functionStack: FunctionStack | undefined;
-let dynamoStack: DynamoStack | undefined;
+
+let iamStack = new IamStack(app, 'IamStack', { env });
+let functionStack = new FunctionStack(app, 'FunctionStack', { env });
+let dynamoStack =   new DynamoStack(app, 'DynamoStack', { env });
+
 // 1) Create IAM stack (defines the Lambda execution role)
 if(controller.stacks.includes("IamStack")){
-const iamStack = new IamStack(app, 'IamStack', { env });
+ iamStack = new IamStack(app, 'IamStack', { env });
 }
 
 // 2) Create Lambda stack, importing the role by ARN
-if(controller.stacks.includes("FunctionStack")){
-const functionStack = new FunctionStack(app, 'FunctionStack', {
+// if(controller.stacks.includes("FunctionStack")){
+ functionStack = new FunctionStack(app, 'FunctionStack', {
   env,  
 });
-if (iamStack) functionStack.addDependency(iamStack);
-}
+// if (iamStack) functionStack.addDependency(iamStack);
+// }
 
 // 3) Create DynamoDB stack
-if(controller.stacks.includes("DynamoStack")){
-const dynamoStack = new DynamoStack(app, 'DynamoStack', { env });
-}
+// if(controller.stacks.includes("DynamoStack")){
+dynamoStack = new DynamoStack(app, 'DynamoStack', { env });
+// }
 
 // Optional but recommended: ensure IAM deploys before Lambda
