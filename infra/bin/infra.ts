b3a0b18e7682985a0e18347c04743f5698a88dd4
Nisarg Patel <nisarg.patel@hitachids.com>
2025-12-22 10:29:47 +0530

    fix infra

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,6 +1,5 @@
-const cdk = require('aws-cdk-lib');
-const { IamStack } = require('../lib/iam_stack');
-const { FunctionStack } = require('../lib/function_stack');
+import * as cdk from 'aws-cdk-lib';
+import { IamStack } from '../lib/iam_stack';
 import controller from './controller.json';
 const app = new cdk.App();
 
@@ -9,20 +8,20 @@ const env = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEFAULT_REGION,
 };
-//
+
 // 1) Create IAM stack (defines the Lambda execution role)
 if(controller.stacks.includes("IamStack")){
 const iamStack = new IamStack(app, 'IamStack', { env });
 }
 
 // 2) Create Lambda stack, importing the role by ARN
-if(controller.stacks.includes("FunctionStack")){
-const functionStack = new FunctionStack(app, 'FunctionStack', {
-  env,
-  // pass only the ARN (NOT the Role object)
-  lambda  lambdaRoleArn: iamStack.lambdaRole.roleArn,
-});
-}
+// if(controller.stacks.includes("FunctionStack")){
+// const functionStack = new FunctionStack(app, 'FunctionStack', {
+//   env,
+//   // pass only the ARN (NOT the Role object)
+//   lambda  lambdaRoleArn: iamStack.lambdaRole.roleArn,
+// });
+// }
 
 
 // Optional but recommended: ensure IAM deploys before Lambda
